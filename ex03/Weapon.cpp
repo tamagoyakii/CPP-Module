2ad5646e0f8b871f7170ace66bdb68dfd7bdab33
tamagoyakii <rlawlgus4063@naver.com>
2023-01-08 15:12:08 +0900

    CPP01

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,13 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type): _type(type) {}
+
+Weapon::~Weapon() {}
+
+const std::string& Weapon::getType() {
+	return _type;
+}
+
+void Weapon::setType(std::string type) {
+	_type = type;
+}
