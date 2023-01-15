d9887d77d7e27f01ed4d978e9b826c3fff9b1b94
tamagoyakii <rlawlgus4063@naver.com>
2023-01-15 17:31:58 +0900

    CPP03

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,7 +1,7 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-	std::cout << "ClapTrap " << name << " created." << std::endl;
+	std::cout << "ClapTrap " << this->_name << " created." << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& ref) : _name(ref._name), _hitPoints(ref._hitPoints), _energyPoints(ref._energyPoints), _attackDamage(ref._attackDamage) {
--- ex02/ClapTrap.cpp
+++ ex02/ClapTrap.cpp
@@ -1,11 +1,11 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-	std::cout << "ClapTrap " << name << " created." << std::endl;
+	std::cout << "ClapTrap " << this->_name << " created." << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name, int hp, int ep, int ad): _name(name), _hitPoints(hp), _energyPoints(ep), _attackDamage(ad) {
-	std::cout << "ClapTrap " << name << " created." << std::endl;
+	std::cout << "ClapTrap " << this->_name << " created." << std::endl;
 };
 
 ClapTrap::ClapTrap(const ClapTrap& ref) : _name(ref._name), _hitPoints(ref._hitPoints), _energyPoints(ref._energyPoints), _attackDamage(ref._attackDamage) {
