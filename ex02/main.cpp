ba6d91e1377f4b98dbd28567b46cdd43369bf721
tamagoyakii <rlawlgus4063@naver.com>
2023-04-18 17:48:11 +0900

    [fix] 함수 간소화

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,38 +1,26 @@
 #include "PmergeMe.hpp"
 
-bool isPositiveInt(const std::string &s) {
-    std::istringstream iss(s);
-    int num;
-
-    iss >> std::noskipws >> num;
-    if (!iss.eof() || iss.fail())
-        return false;
-
-    return num <= std::numeric_limits<int>::max() && num > 0;
-}
-
 int main(int ac, char **av) {
     if (ac < 2) {
         std::cout << "Error: need more arguments" << std::endl;
         exit(EXIT_FAILURE);
     } else if (ac > 3001) {
-        std::cout << "Error: Too many arguments" << std::endl;
+        std::cout << "Error: too many arguments" << std::endl;
         exit(EXIT_FAILURE);
     }
 
     PmergeMe m;
 
     for (int i = 1; i < ac; i++) {
-        if (isPositiveInt(av[i])) {
-            std::istringstream iss(av[i]);
-            int num;
+        std::istringstream iss(av[i]);
+        int num;
 
-            iss >> num;
-            m.addNumber(num);
-        } else {
+        iss >> num;
+        if (!iss.eof() || iss.fail() || num <= 0 || num > std::numeric_limits<int>::max()) {
             std::cout << "Error" << std::endl;
             exit(EXIT_FAILURE);
         }
+        m.addNumber(num);
     }
     m.setSize(ac - 1);
     m.execute();
